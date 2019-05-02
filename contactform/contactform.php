3ed4017ac592e8135df67862c00d1e21f2356b0a
mekjr1 <gafar_meque@hotmail.com>
2019-05-02 17:56:51 +0200

    email php script updated

---
+++ contactform/contactform.php
@@ -0,0 +1,47 @@
+<?php
+ 
+if($_POST) {
+    $visitor_name = "";
+    $visitor_email = "";
+    $email_title = "";
+    $concerned_department = "";
+    $visitor_message = "";
+     
+    if(isset($_POST['name'])) {
+        $visitor_name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    }
+     
+    if(isset($_POST['email'])) {
+        $visitor_email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
+        $visitor_email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
+    }
+     
+ 
+     
+    if(isset($_POST['subject'])) {
+        $concerned_department = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
+    }
+     
+    if(isset($_POST['visitor_message'])) {
+        $visitor_message = htmlspecialchars($_POST['visitor_message']);
+    }
+     
+    
+        $recipient = "info@nmtss.net";
+    
+     
+    $headers  = 'MIME-Version: 1.0' . "\r\n"
+    .'Content-type: text/html; charset=utf-8' . "\r\n"
+    .'From: ' . $visitor_email . "\r\n";
+     
+    if(mail($recipient, $email_title, $visitor_message, $headers)) {
+        echo "OK";
+    } else {
+        echo '<p>We are sorry but the email did not go through.</p>';
+    }
+     
+} else {
+    echo '<p>Something went wrong</p>';
+}
+ 
+?>
